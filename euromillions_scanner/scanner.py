46a7055390c1e173a62168f5f3d46fb0e4f349d0
João Mateus <jpcmateus@gmail.com>
2014-08-04 16:27:15 +0100

    Added the code necessary to find the widest contours.

--- euromillions_scanner/scanner.py
+++ euromillions_scanner/scanner.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 class Ticket:
@@ -58,11 +59,84 @@ def create_binary_image(image_path, max_size):
     return binary_image
 
 
+def invert_image(image):
+    """
+        Inverts a binary image
+
+        Args:
+            image: a binary image (black and white only)
+
+        Returns:
+            An inverted version of the image passed as argument
+    """
+    return 255 - image
+
+
+def find_widest_contours(image, num_contours, dilation_scale=8):
+    """
+        Computes the widest contours of the image passed as parameter
+
+        Args:
+            image: the image we want to find the contours. the image should be
+                binary, with black background and white foreground.
+            num_contours: the number of contours we want to obtain
+            dilation_scale: size of the kernel use to dilate the image
+                horizontally. For example, if the dilation_scale is 8, the
+                kernel is 8x8, with 3 middle rows with ones and the rest zeros
+
+        Returns:
+            The num_contours widest contours of the image
+    """
+    # TODO: try solving this using the Hough transform algorithm
+
+    # Create the dilation matrix
+    dilation_mat = np.zeros((dilation_scale, dilation_scale), np.uint8)
+    dilation_mat[dilation_scale / 2 - 1:dilation_scale / 2 + 2] = 1
+
+    # Create a copy because findContours modifies the image
+    dilated_image = cv2.dilate(image, dilation_mat).copy()
+    contours, _ = cv2.findContours(dilated_image,
+                                   cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_NONE)
+
+    # Get the bounding rectangles of the countours
+    contours_rects = map(cv2.boundingRect, contours)
+    contours_rects.sort(key=lambda rect: rect[2])  # Sort by width
+    contours_rects.reverse()  # Descending order
+
+    return contours_rects[:num_contours]
+
+
 def show_image(image, wait_time=0, window_name='Image'):
+    """
+        Display an image using OpenCV
+
+        Args:
+            image: image we want to display
+            wait_time: how long (in milliseconds) should the image be displayed
+            window_name: name of the window (defaults to 'Image')
+    """
     cv2.imshow(window_name, image)
     cv2.waitKey(wait_time)
 
 
+def _main():
+    img = create_binary_image('test/data/euro.jpg', (600, 600))
+    inverted_img = invert_image(img)
+    show_image(inverted_img, 2000)
+
+    contours = find_widest_contours(inverted_img, 2)
+
+    # Colored image used to draw the contours
+    color_img = cv2.cvtColor(inverted_img, cv2.COLOR_GRAY2BGR)
+    for rect in contours:
+        point1 = (rect[0], rect[1])
+        point2 = (rect[0] + rect[2], rect[1] + rect[3])
+
+        cv2.rectangle(color_img, point1, point2, (0, 255, 0), 1)
+
+    show_image(color_img)
+
+
 if __name__ == '__main__':
-    img = create_binary_image('test/data/euro2.jpg', (600, 600))
-    show_image(img)
+    _main()
